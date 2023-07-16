2e72b9f89edbee079856b8646f0a69bf323bee38
AizazNaeem4433 <139700180+AizazNaeem4433@users.noreply.github.com>
2023-07-16 23:22:01 +0500

    Add files via upload

---
+++ Assignment1.js
@@ -0,0 +1,4 @@
+"use strict";
+let myfriend = "Ali";
+// used backtickes and ${} to assign the variable to myfriend string.
+console.log(`Hello ${myfriend} .\n would you like to play somthing with me.`);
