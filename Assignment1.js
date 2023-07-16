cfdbd62a0121c949bae826baa11674089f480e4d
AizazNaeem4433 <139700180+AizazNaeem4433@users.noreply.github.com>
2023-07-16 23:25:57 +0500

    Delete Assignment1.js

--- Assignment1.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-let myfriend = "Ali";
-// used backtickes and ${} to assign the variable to myfriend string.
-console.log(`Hello ${myfriend} .\n would you like to play somthing with me.`);
