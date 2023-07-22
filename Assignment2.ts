37c32b4371ac9019313dcb8587e9cd8512560a1b
AizazNaeem4433 <139700180+AizazNaeem4433@users.noreply.github.com>
2023-07-23 00:55:52 +0500

    Add files via upload

---
+++ Assignment2.ts
@@ -0,0 +1,11 @@
+
+let name = "Muhammad Aizaz naeem" ;
+
+let firstname = "Muhammad Aizaz";
+
+let lastname ="naeem";
+
+let fullname = `${firstname} ${lastname}`;
+
+console.log(fullname.toLowerCase());
+console.log(fullname.toUpperCase());
