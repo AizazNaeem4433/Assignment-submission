11753c585fa2c3bcdb5db2a058d5fc3b1ae81fc1
AizazNaeem4433 <139700180+AizazNaeem4433@users.noreply.github.com>
2023-07-23 01:18:45 +0500

    Add files via upload

---
+++ Assignment3.ts
@@ -0,0 +1,5 @@
+let Authorname = "Allama Muammmad Iqbal ";
+let message = 'said."Failure is not fatal until we surrender; trying again is the key to glorious victory"';
+
+
+console.log(Authorname,message);
