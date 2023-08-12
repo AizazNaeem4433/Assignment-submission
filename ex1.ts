6a01826e59b356eb5cbe33497cd735a1b9214bbb
AizazNaeem4433 <139700180+AizazNaeem4433@users.noreply.github.com>
2023-08-13 03:49:49 +0500

    Add files via upload

---
+++ ex1.ts
@@ -0,0 +1,11 @@
+//exercise 1 : write a program that calculates the area of a ractangle
+
+function Areaofractangle(length: number , width: number) : number {return length * width};
+
+const lengths = 50;
+const width = 10 ;
+const Area = 
+Areaofractangle(lengths,width);
+
+console.log(`the area of the ractangle is : ${Area}`);
+
